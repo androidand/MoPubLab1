fef7309c3c8c80ed177d614cbec6a83f7908ed83
androidand <sylvan@kth.se>
2014-03-27 16:46:15 +0100

    Fixade lite med css
    Nu funkar Twitter bootstrap.

---
+++ js/ControlPanelController.js
@@ -0,0 +1,18 @@
+
+
+$( document ).ready(function() {
+//console.log( "ready!" );
+	//$('#controlPanel').hide();
+
+      $( "#PanUp" ).click(function() {
+		//alert( "Handler for .click() called." );
+		update(10);
+
+		});
+
+
+});
+	/* $('#controlPanel').on('click', '#PanUp', function(){
+				alert("Hej");
+	});
+*/
--- js/googleAPI.js
+++ js/googleAPI.js
@@ -1,10 +1,32 @@
+      //window.myZoom = 20;
       function initialize() {
+        
         var mapOptions = {
           center: new google.maps.LatLng(59.3482, 18.0723),
-          zoom: 5,
+          zoom: 20,
           mapTypeId: google.maps.MapTypeId.TERRAIN 
         };
+
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+        //document.getElementById("map-canvas")
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
+
+      function update(zoom){
+      //mapOptions.zoom = zoom;
+      map.setZoom(6);
+      //google.maps
+      //alert("Update!");
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize, update);
+
+
+ 
+
+      google.maps.event.addDomListener($('#controlPanel'), 'click', function() {
+      //map.setCenter(chicago)
+      map.setZoom(6);  
+      });
+
+
